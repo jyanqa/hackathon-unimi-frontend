7824943f9c8952f0a1d81d4480337900f8ccb726
Nam Nguyen <nhnam1105@gmail.com>
2021-05-09 16:39:50 +0200

    port check

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    request = requests.get('http://localhost:5000/api/get_all_coinpairs')
+    request = requests.get('http://127.0.0.1:5000/api/get_all_coinpairs')
     list_crypto_pairs = json.loads(request.text) #sorted
     #list_crypto = list_crypto_pairs.sort()
     extra_info = ['last price', 'volume']
