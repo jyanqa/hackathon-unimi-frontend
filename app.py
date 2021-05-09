254d9a768267a395bb4699fd90f46d73f0cbd6cc
Nam Nguyen <nhnam1105@gmail.com>
2021-05-09 13:59:01 +0200

    d

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 
 from flask_ngrok import run_with_ngrok
 app = Flask(__name__)
-run_with_ngrok(app)
+
 @app.route('/')
 def index():
     request = requests.get('http://127.0.0.1:5000/api/get_all_coinpairs')
@@ -14,4 +14,4 @@ def index():
     return render_template('index.html', colours = list_crypto_pairs, extra_info=extra_info)
 
 
-app.run(host="localhost", port=8000, debug=True)
+app.run(host="localhost", port=7000, debug=True)
