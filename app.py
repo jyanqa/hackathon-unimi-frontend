ae3b102d2d4241f01f6d6fbecd4545edf76ce9dd
NGUYEN QA <59968046+jyanqa@users.noreply.github.com>
2021-05-09 13:58:09 +0200

    candlestick

--- app.py
+++ app.py
@@ -7,10 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     request = requests.get('http://0.0.0.0:8000/api/get_all_coinpairs')
-    list_crypto_pairs = json.loads(request.text) #sorted
-    #list_crypto = list_crypto_pairs.sort()
-    extra_info = ['last price', 'volume']
+    list_crypto_pairs = json.loads(request.text)
+    extra_info = ['volume', 'Candlestick Charts']
     return render_template('index.html', colours = list_crypto_pairs,extra_info=extra_info)
-
 if __name__ == "__main__":
     app.run(host="localhost", port=80, debug=True)
